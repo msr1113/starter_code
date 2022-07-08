d3f1585e7bc9bb592cfee900b8f7010157440c7b
mrawal <imsr1013@gmail.com>
2022-07-08 16:19:19 +0545

    creating unit test for item which will test whether we can save it to database and remove from database

--- tests/base_test.py
+++ tests/base_test.py
@@ -10,6 +10,7 @@ from unittest import TestCase
 from app import app
 from db import db
 
+
 class BaseTest(TestCase):
     def setUp(self):
         # make sure database exists
@@ -21,10 +22,8 @@ class BaseTest(TestCase):
         self.app = app.test_client()
         self.app_context = app.app_context
 
-
     def tearDown(self):
-        #database is blank
+        # database is blank
         with app.app_context():
             db.session.remove()
             db.drop_all()
-
